ea5d9176bcd98642016ff3008145756a1e5e045a
aswincj <aswincjmenon@gmail.com>
2017-10-17 01:56:02 +0530

    server

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(_dirname +'/dist'));
+app.use(express.static(__dirname + '/src'));
 app.listen(process.env.PORT || 8080);
 
 //pathlocationstratergy
-app.get('/*', function(req, res){
-	res.sendFile(path.join(_dirname + '/dist/index.html'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 })
 console.log('console listening');
-
-
