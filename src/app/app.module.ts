738099b25e7a2f491f8a4c1c4bdb53c5f9aa7601
aswincj <aswincjmenon@gmail.com>
2017-10-17 01:04:30 +0530

    all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+      bootstrap: [/*AppComponent,*/TestComponent]
 })
 export class AppModule { }
